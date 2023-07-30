28aa5420c3097898c57adc929ddbe6f728590c02
z.hariri <z.hariry@gmail.com>
2023-07-30 14:36:05 +0330

    initial commit

---
+++ src/test/java/com/example/authenticator/AuthenticatorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorApplicationTests {
+
+    @Test
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticatorApplication.class, args);
+    }
+
+}
